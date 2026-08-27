1dcf4fcbdcae0029c2d431033133af03f0cd9a9e
Raihan A. <266201844+raihan-rifat007@users.noreply.github.com>
2026-08-27 19:17:14 +0600

    Upgradeeeeeeeeeeee to next level

--- ui.ts
+++ ui.ts
@@ -3,7 +3,7 @@ export const html = `<!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>YouTube API</title>
+  <title>YouTube Music API</title>
   <link rel="icon" href="/assets/logo.png">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
   <style>
